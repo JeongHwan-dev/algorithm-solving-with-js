c609f3bcf04e1551e0235cf8daa741e596a71a4f
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-09-19 18:52:16 +0900

    Solve : 소수 만들기

---
+++ Level1/소수만들기.js
@@ -0,0 +1,39 @@
+function isPrime(n) {
+  if (n === 1) {
+    return false;
+  }
+
+  for (let i = 2; i <= parseInt(Math.sqrt(n)); i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function solution(nums) {
+  let answer = 0;
+  let n = nums.length;
+  let m = 3;
+  let tmp = Array.from({ length: m }, () => 0);
+
+  function DFS(L, s) {
+    if (L === m) {
+      const sum = tmp.reduce((acc, cur) => acc + cur, 0);
+
+      if (isPrime(sum)) {
+        answer++;
+      }
+    } else {
+      for (let i = s; i < n; i++) {
+        tmp[L] = nums[i];
+        DFS(L + 1, i + 1);
+      }
+    }
+  }
+
+  DFS(0, 0);
+
+  return answer;
+}
