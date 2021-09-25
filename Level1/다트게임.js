d84834dcb52765dc9ca97a2527781b168a015819
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-09-25 16:49:21 +0900

    Add : '다트 게임' 솔루션 추가

--- Level1/다트게임.js
+++ Level1/다트게임.js
@@ -1,4 +1,6 @@
-function solution(dartResult) {
+// Solution 1
+// if 문을 이용한 솔루션
+function solution1(dartResult) {
   let totalScore = 0;
   const darts = dartResult.split('');
   const stack = [];
@@ -33,6 +35,52 @@ function solution(dartResult) {
       } else if (e === '#') {
         stack.push(stack.pop() * -1);
       }
+    }
+  });
+
+  totalScore = stack.reduce((acc, cur) => acc + cur, 0);
+
+  return totalScore;
+}
+
+// Solution 2
+// switch 문을 이용한 솔루션
+function solution2(dartResult) {
+  let totalScore = 0;
+  const darts = dartResult.split('');
+  const stack = [];
+
+  for (let i = 0; i < darts.length - 1; i++) {
+    if (darts[i] === '1' && darts[i + 1] === '0') {
+      darts.splice(i, 2, '10');
+    }
+  }
+
+  darts.forEach((e) => {
+    switch (e) {
+      case 'S':
+        break;
+      case 'D':
+        stack.push(Math.pow(stack.pop(), 2));
+        break;
+      case 'T':
+        stack.push(Math.pow(stack.pop(), 3));
+        break;
+      case '*':
+        if (stack.length === 1) {
+          stack.push(stack.pop() * 2);
+        } else {
+          const cur = stack.pop() * 2;
+          const prev = stack.pop() * 2;
+
+          stack.push(...[prev, cur]);
+        }
+        break;
+      case '#':
+        stack.push(stack.pop() * -1);
+        break;
+      default:
+        stack.push(Number(e));
     }
   });
 
