ba6825bc4c20902d5db6df96a93570a6d93035b2
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-10-22 13:21:28 +0900

    Add : '숫자 게임' 솔루션 코드 추가

--- Level3/숫자게임.js
+++ Level3/숫자게임.js
@@ -1,4 +1,6 @@
-function solution(A, B) {
+// Solution 1
+// forEach() 메서드를 활용한 솔루션
+function solution1(A, B) {
   let score = 0;
   let i = 0;
 
@@ -11,6 +13,23 @@ function solution(A, B) {
       i++;
     }
   });
+
+  return score;
+}
+
+// Solution 2
+// filter() 메서드를 활용한 솔루션
+function solution2(A, B) {
+  A.sort((a, b) => a - b);
+  B.sort((a, b) => a - b);
+
+  let i = 0;
+  const score = B.filter((num) => {
+    if (num > A[i]) {
+      i++;
+      return num;
+    }
+  }).length;
 
   return score;
 }
