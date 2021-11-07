2c87b260a2aaa91300cd9a7a4801dd99c8b61779
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-11-07 20:37:07 +0900

    Solve : 평균 구하기

--- Level1/평균구하기.js
+++ Level1/평균구하기.js
@@ -1,4 +1,5 @@
-function solution(arr) {
+// Solution 1
+function solution1(arr) {
   let answer = 0;
   let sum = 0;
 
@@ -9,4 +10,20 @@ function solution(arr) {
   answer = sum / arr.length;
 
   return answer;
+}
+
+// Solution 2
+function solution2(arr) {
+  const n = arr.length;
+  const sum = arr.reduce((acc, cur) => acc + cur);
+  const average = sum / n;
+
+  return average;
+}
+
+// Solution 3
+function solution3(arr) {
+  const average = arr.reduce((acc, cur) => acc + cur) / arr.length;
+
+  return average;
 }
