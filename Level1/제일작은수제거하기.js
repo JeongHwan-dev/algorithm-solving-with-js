84b6572119fa94e8aed30280b575d8c0d90b5f69
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-11-08 09:40:51 +0900

    Solve : 제일 작은 수 제거하기

--- Level1/제일작은수제거하기.js
+++ Level1/제일작은수제거하기.js
@@ -1,4 +1,5 @@
-function solution(arr) {
+// Solution 1
+function solution1(arr) {
   let answer = [];
   let minIdx = 0;
   let min = arr[minIdx];
@@ -20,5 +21,17 @@ function solution(arr) {
   return answer;
 }
 
-let arr = [10];
-console.log(solution(arr));
+// Solution 2
+function solution2(arr) {
+  const _arr = arr.slice();
+  const minNum = Math.min(..._arr);
+  const minNumIndex = _arr.indexOf(minNum);
+
+  _arr.splice(minNumIndex, 1);
+
+  if (_arr.length === 0) {
+    _arr.push(-1);
+  }
+
+  return _arr;
+}
