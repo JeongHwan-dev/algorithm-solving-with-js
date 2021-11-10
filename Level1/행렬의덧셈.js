b6d8f6443a3af9748488fd2c2ab253aded48a89d
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-11-10 22:56:25 +0900

    Solve : 행렬의 덧셈

--- Level1/행렬의덧셈.js
+++ Level1/행렬의덧셈.js
@@ -1,15 +1,25 @@
-function solution(arr1, arr2) {
-  let answer = [];
+// Solution 1
+function solution1(arr1, arr2) {
+  const sumMatrix = [];
 
   for (let r = 0; r < arr1.length; r++) {
-    let tmp = [];
+    const temp = [];
 
     for (let c = 0; c < arr1[0].length; c++) {
-      tmp.push(arr1[r][c] + arr2[r][c]);
+      temp.push(arr1[r][c] + arr2[r][c]);
     }
 
-    answer.push(tmp);
+    sumMatrix.push(temp);
   }
 
-  return answer;
+  return sumMatrix;
+}
+
+// Solution 2
+function solution2(arr1, arr2) {
+  const sumMatrix = arr1.map((row, rowIndex) =>
+    row.map((num, columnIndex) => num + arr2[rowIndex][columnIndex])
+  );
+
+  return sumMatrix;
 }
