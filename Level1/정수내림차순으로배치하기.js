a4c063bfd12ca337457d3d212903530d02243209
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-11-11 20:58:32 +0900

    Solve : 정수 내림차순으로 정렬하기

--- Level1/정수내림차순으로배치하기.js
+++ Level1/정수내림차순으로배치하기.js
@@ -1,12 +1,27 @@
-function solution(n) {
-  let answer;
-  let arr = n
+// Solution 1
+function solution1(n) {
+  const arr = n
     .toString()
     .split('')
     .map((num) => Number(num));
 
   arr.sort((a, b) => b - a);
-  answer = Number(arr.join(''));
 
-  return answer;
+  const descNums = Number(arr.join(''));
+
+  return descNums;
+}
+
+// Solution 2
+function solution2(n) {
+  const descNums = parseInt(
+    n
+      .toString()
+      .split('')
+      .map((num) => parseInt(num))
+      .sort((a, b) => b - a)
+      .join('')
+  );
+
+  return descNums;
 }
