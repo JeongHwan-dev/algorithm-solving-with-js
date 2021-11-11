d3e0b0554ea4ccf3c38bdc18957a7fcacb00dd0b
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-11-11 20:52:20 +0900

    Solve : x만큼 간격이 있는 n개의 숫자

--- Level1/x만큼간격이있는n개의숫자.js
+++ Level1/x만큼간격이있는n개의숫자.js
@@ -1,11 +1,19 @@
-function solution(x, n) {
-  let answer = [];
+// Solution 1
+function solution1(x, n) {
+  const result = [];
 
-  answer.push(x);
+  result.push(x);
 
   for (let i = 0; i < n - 1; i++) {
-    answer.push(answer[i] + x);
+    result.push(result[i] + x);
   }
 
-  return answer;
+  return result;
+}
+
+// Solution 2
+function solution2(x, n) {
+  const result = Array.from({ length: n }, (_, index) => x * (index + 1));
+
+  return result;
 }
