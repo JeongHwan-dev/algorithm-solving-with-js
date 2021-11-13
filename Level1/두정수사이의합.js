59841168638e861af84ea235019197e1ce009df7
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-11-13 10:49:53 +0900

    Solve : 두 정수 사이의 합

--- Level1/두정수사이의합.js
+++ Level1/두정수사이의합.js
@@ -1,11 +1,33 @@
-function solution(a, b) {
-  let answer = 0;
-  let max = Math.max(a, b);
-  let min = Math.min(a, b);
+// Solution 1
+function solution1(a, b) {
+  const minNum = Math.min(a, b);
+  const maxNum = Math.max(a, b);
+  let sum = 0;
 
-  for (let i = min; i <= max; i++) {
-    answer += i;
+  for (let num = minNum; num <= maxNum; num++) {
+    sum += num;
   }
 
-  return answer;
+  return sum;
+}
+
+// Solution 2
+function getSumFromOneToN(num) {
+  const result = (num * (num + 1)) / 2;
+
+  return result;
+}
+
+function solution(a, b) {
+  let result;
+
+  if (a === b) {
+    result = a;
+  } else if (a > b) {
+    result = getSumFromOneToN(a) - getSumFromOneToN(b - 1);
+  } else {
+    result = getSumFromOneToN(b) - getSumFromOneToN(a - 1);
+  }
+
+  return result;
 }
