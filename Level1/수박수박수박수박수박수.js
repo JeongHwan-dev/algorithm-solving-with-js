7812214b49d9f146cc33380cac0083e5f09fd046
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-11-14 09:42:34 +0900

    Solve : 수박수박수박수박수박수?

--- Level1/수박수박수박수박수박수.js
+++ Level1/수박수박수박수박수박수.js
@@ -1,24 +1,31 @@
+// Solution 1
 function solution1(n) {
-  let answer = '';
+  let result = '';
 
   for (let i = 0; i < n; i++) {
-    if (i % 2 === 0) {
-      answer += '수';
-    } else {
-      answer += '박';
-    }
+    i % 2 === 0 ? (result += '수') : (result += '박');
   }
 
-  return answer;
+  return result;
 }
 
+// Solution 2
 function solution2(n) {
   const repeatCnt = Math.floor(n / 2);
-  let answer = '수박'.repeat(repeatCnt);
+  let result = '수박'.repeat(repeatCnt);
 
   if (n % 2) {
-    answer += '수';
+    result += '수';
   }
 
-  return answer;
+  return result;
+}
+
+// Solution 3
+function solution3(n) {
+  const result = Array.from({ length: n }, (_, index) =>
+    index % 2 === 0 ? '수' : '박'
+  ).join('');
+
+  return result;
 }
