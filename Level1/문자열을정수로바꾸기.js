736b167f053e10033e4db46893cc020a049f7d5a
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-11-20 11:08:00 +0900

    Solve : 문자열을 정수로 바꾸기

--- Level1/문자열을정수로바꾸기.js
+++ Level1/문자열을정수로바꾸기.js
@@ -1,9 +1,10 @@
+// Solution 1
 function solution1(s) {
   let answer = 0;
 
   if (isNaN(s.slice(0, 1))) {
-    let sign = s.slice(0, 1);
-    let num = Number(s.slice(1));
+    const sign = s.slice(0, 1);
+    const num = Number(s.slice(1));
 
     if (sign === '-') {
       answer = 0 - num;
@@ -17,10 +18,12 @@ function solution1(s) {
   return answer;
 }
 
+// Solution 2
 function solution2(s) {
-  return Number(s);
+  return parseInt(s);
 }
 
+// Solution 3
 function solution3(s) {
-  return parseInt(s);
+  return Number(s);
 }
