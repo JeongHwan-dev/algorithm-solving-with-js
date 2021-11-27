0981c7983e9c38b0284d4a69572df8bee192206e
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-11-27 14:15:30 +0900

    Solve : 시저 암호

--- Level1/시저암호.js
+++ Level1/시저암호.js
@@ -1,59 +1,57 @@
-function solution(s, n) {
-  let answer = '';
-  let arr = s.split('');
+// Solution 1
+function solution1(s, n) {
+  let array = s.split('');
 
   for (let i = 0; i < s.length; i++) {
     let asc = s.charCodeAt(i);
 
     if (asc >= 65 && asc <= 90) {
       if (asc + n > 90) {
-        arr[i] = 64 + (asc + n) - 90;
+        array[i] = 64 + (asc + n) - 90;
       } else {
-        arr[i] = asc + n;
+        array[i] = asc + n;
       }
     } else if (asc >= 97 && asc <= 122) {
       if (asc + n > 122) {
-        arr[i] = 96 + (asc + n) - 122;
+        array[i] = 96 + (asc + n) - 122;
       } else {
-        arr[i] = asc + n;
+        array[i] = asc + n;
       }
     } else {
-      arr[i] = asc;
+      array[i] = asc;
     }
   }
 
-  answer = arr.map((e) => String.fromCharCode(e)).join('');
+  const result = array.map((e) => String.fromCharCode(e)).join('');
 
-  return answer;
+  return result;
 }
 
+// Solution 2
 function solution2(s, n) {
   const lowerAlphabet = 'abcdefghijklmnopqrstuvwxyz';
   const upperAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  let answer;
-  let arr = [];
+  const array = [];
 
   for (let i = 0; i < s.length; i++) {
     let nextIdx;
 
     if (lowerAlphabet.indexOf(s[i]) !== -1) {
       nextIdx = (lowerAlphabet.indexOf(s[i]) + n) % 26;
-      console.log(nextIdx);
-      arr.push(lowerAlphabet[nextIdx]);
+      array.push(lowerAlphabet[nextIdx]);
     }
 
     if (upperAlphabet.indexOf(s[i]) !== -1) {
       nextIdx = (upperAlphabet.indexOf(s[i]) + n) % 26;
-      console.log(nextIdx);
-      arr.push(upperAlphabet[nextIdx]);
+      array.push(upperAlphabet[nextIdx]);
     }
 
     if (s[i] === ' ') {
-      arr.push(' ');
+      array.push(' ');
     }
   }
 
-  answer = arr.join('');
+  const result = array.join('');
 
-  return answer;
+  return result;
 }
