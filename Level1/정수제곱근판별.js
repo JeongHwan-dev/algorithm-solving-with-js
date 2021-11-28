74b457617a75177b929c0d72054c5446211177fe
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-11-28 16:48:06 +0900

    Solve : 정수 제곱근 판별

--- Level1/정수제곱근판별.js
+++ Level1/정수제곱근판별.js
@@ -1,18 +1,36 @@
+// Solution 1
 function solution1(n) {
-  let answer = 0;
+  let result = 0;
 
   if (Number.isInteger(Math.sqrt(n))) {
-    answer = Math.pow(Math.sqrt(n) + 1, 2);
+    result = Math.pow(Math.sqrt(n) + 1, 2);
   } else {
-    answer = -1;
+    result = -1;
   }
 
-  return answer;
+  return result;
 }
 
+// Solution 2
 function solution2(n) {
   const sqrtN = Math.sqrt(n);
-  const answer = Number.isInteger(sqrtN) ? Math.pow(sqrtN + 1, 2) : -1;
+  const result = Number.isInteger(sqrtN) ? Math.pow(sqrtN + 1, 2) : -1;
 
-  return answer;
+  return result;
+}
+
+// Solution 3
+function solution3(n) {
+  const result = Number.isInteger(Math.sqrt(n))
+    ? Math.pow(Math.sqrt(n) + 1, 2)
+    : -1;
+
+  return result;
+}
+
+// Solution 4
+function solution4(n) {
+  const result = Math.sqrt(n) % 1 === 0 ? Math.pow(Math.sqrt(n) + 1, 2) : -1;
+
+  return result;
 }
