84193fc1ed51070f2563b8f2428676df212d6898
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-11-29 15:20:41 +0900

    Solve : 문자열 내 마음대로 정렬하기

--- Level1/문자열내마음대로정렬하기.js
+++ Level1/문자열내마음대로정렬하기.js
@@ -1,12 +1,26 @@
-function solution(strings, n) {
-  strings.sort((a, b) => {
-    let aValue = a[n];
-    let bValue = b[n];
+// Solution 1
+function solution1(strings, n) {
+  const _strings = strings.slice();
+
+  _strings.sort((a, b) => {
+    const aValue = a[n];
+    const bValue = b[n];
 
     return aValue === bValue
       ? (a > b) - (a < b)
       : (aValue > bValue) - (aValue < bValue);
   });
 
-  return strings;
+  return _strings;
+}
+
+// Solution 2
+function solution2(strings, n) {
+  const _strings = strings.slice();
+
+  _strings.sort((a, b) =>
+    a[n] === b[n] ? a.localeCompare(b) : a[n].localeCompare(b[n])
+  );
+
+  return _strings;
 }
