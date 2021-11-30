d25686d230e93534082455f4a0337ea17b178889
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-11-30 17:03:23 +0900

    Solve : K번째수

--- Level1/K번째수.js
+++ Level1/K번째수.js
@@ -1,70 +1,44 @@
-// for 문을 사용한 솔루션 1
+// Solution 1
 function solution1(array, commands) {
-  let answer = [];
+  const result = [];
 
-  // for문을 사용해 명령문 탐색
   for (let i = 0; i < commands.length; i++) {
-    // 시작점, 끝점, 타겟 인덱스 변수에 값 할당
     const start = commands[i][0] - 1;
     const end = commands[i][1];
     const idx = commands[i][2] - 1;
 
-    // 요구하는 범위만큼 잘라온 후 임시 배열에 저장
-    let tmp = array.slice(start, end);
+    const temp = array.slice(start, end);
 
-    // 배열 오름차순으로 정렬
-    tmp.sort((a, b) => a - b);
+    temp.sort((a, b) => a - b);
 
-    // 타켓 값 불러온 후 결과 배열에 push
-    answer.push(tmp[idx]);
+    result.push(temp[idx]);
   }
 
-  return answer;
+  return result;
 }
 
-// map()을 사용한 솔루션 2
+// Solution 2
 function solution2(array, commands) {
-  // map() 메서드를 사용해 명령문 탐색하고 새로운 배열 만들기
-  let answer = commands.map((command) => {
-    // 시작점, 끝점, 타겟 인덱스 변수에 값 할당
+  const result = commands.map((command) => {
     const start = command[0] - 1;
     const end = command[1];
     const idx = command[2] - 1;
+    const tmp = array.slice(start, end).sort((a, b) => a - b);
 
-    // 요구하는 범위만큼 잘라온 후 오름차순으로 배열해서 임시 배열에 저장
-    let tmp = array.slice(start, end).sort((a, b) => a - b);
-
-    // 타겟 값을 리턴
     return tmp[idx];
   });
 
-  return answer;
+  return result;
 }
 
-// 솔루션 2 코드를 개선한 솔루션 3
+// Solution 3
 function solution3(array, commands) {
-  // map() 메서드를 사용해 명령문 탐색하고 새로운 배열 만들기
-  let answer = commands.map((command) => {
-    // 시작점, 끝점, 타겟 인덱스 변수에 값 할당
-    const [start, end, idx] = command;
+  const result = commands.map((command) => {
+    const [start, end, order] = command;
+    const temp = array.slice(start - 1, end).sort((a, b) => a - b);
 
-    // 요구하는 범위만큼 잘라온 후 오름차순으로 배열해서 임시 배열에 저장
-    let tmp = array.slice(start - 1, end).sort((a, b) => a - b);
-
-    // 타겟 값을 리턴
-    return tmp[idx - 1];
+    return temp[order - 1];
   });
 
-  return answer;
+  return result;
 }
-
-let array = [1, 5, 2, 6, 3, 7, 4];
-let commands = [
-  [2, 5, 3],
-  [4, 4, 1],
-  [1, 7, 3],
-];
-
-console.log(solution1(array, commands));
-console.log(solution2(array, commands));
-console.log(solution3(array, commands));
