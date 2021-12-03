513442530eed30b922caa2a2653a0c19e3b1940f
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-12-03 12:55:54 +0900

    Solve : 음양 더하기

--- Level1/음양더하기.js
+++ Level1/음양더하기.js
@@ -1,9 +1,7 @@
-// for문을 이용한 솔루션 1
+// Solution 1
 function solution1(absolutes, signs) {
-  // 합계를 0으로 초기화
   let sum = 0;
 
-  // 정수 배열을 순회하면서 부호 확인 후 더하기 또는 빼기 결정
   for (let i = 0; i < absolutes.length; i++) {
     if (signs[i]) {
       sum += absolutes[i];
@@ -15,25 +13,20 @@ function solution1(absolutes, signs) {
   return sum;
 }
 
-// reduce() 메서드를 이용한 솔루션 2
+// Solution 2
 function solution2(absolutes, signs) {
-  // 정수 배열을 순회하면서 부호 확인 후 더하기 또는 빼기 결정
-  let sum = absolutes.reduce((acc, val, idx) => {
-    if (signs[idx++]) {
-      return acc + val;
-    } else {
-      return acc - val;
-    }
-  }, 0);
+  const sum = absolutes.reduce(
+    (acc, cur, index) => acc + cur * (signs[index] ? 1 : -1),
+    0
+  );
 
   return sum;
 }
 
-// reduce() 메서드를 이용한 솔루션 3
+// Solution 3
 function solution3(absolutes, signs) {
-  // 정수 배열을 순회하면서 부호 확인 후 더하기 또는 빼기 결정
-  let sum = absolutes.reduce(
-    (acc, val, idx) => acc + val * (signs[idx] ? 1 : -1),
+  const sum = absolutes.reduce(
+    (acc, cur, index) => (signs[index] ? acc + cur : acc - cur),
     0
   );
 
