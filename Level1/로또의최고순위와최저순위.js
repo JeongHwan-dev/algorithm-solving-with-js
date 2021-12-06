3f33e7974599e41efd103706ff9960ff7cb97696
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-12-06 14:41:59 +0900

    Solve : 로또의 최고 순위와 최저 순위

--- Level1/로또의최고순위와최저순위.js
+++ Level1/로또의최고순위와최저순위.js
@@ -1,4 +1,4 @@
-// 순위를 반환하는 함수
+// Solution 1
 function getRank(count) {
   let rank;
 
@@ -19,31 +19,64 @@ function getRank(count) {
   return rank;
 }
 
-// 순위를 반환하는 함수를 이용한 솔루션1
 function solution1(lottos, win_nums) {
-  // 0이 없는 로또 배열 생성
   const noZeroLottos = lottos.filter((num) => num !== 0);
-  // 번호가 일치하는 수
   const sameCnt = noZeroLottos.filter((num) => win_nums.includes(num)).length;
-  // 번호가 0인 수
   const zeroCnt = lottos.length - noZeroLottos.length;
-  // getRank() 함수를 이용해 순위 최대, 최소 순위 반환
-  const answer = [getRank(sameCnt + zeroCnt), getRank(sameCnt)];
+  const result = [getRank(sameCnt + zeroCnt), getRank(sameCnt)];
 
-  return answer;
+  return result;
 }
 
-// filter() 메서드와 삼항 조건 연산자를 이용한 솔루션2
+// Solution 2
 function solution2(lottos, win_nums) {
-  const answer = [];
-  // 최소로 번호가 일치하는 수
+  const result = [];
   const min = lottos.filter((n) => win_nums.includes(n)).length;
-  // 최대로 번호가 일치하는 수
   const max = lottos.filter((n) => n === 0).length + min;
 
-  // 일치하는 수에 따른 순위 결과 배열에 push
-  max > 1 ? answer.push(7 - max) : answer.push(6);
-  min > 1 ? answer.push(7 - min) : answer.push(6);
+  max > 1 ? result.push(7 - max) : result.push(6);
+  min > 1 ? result.push(7 - min) : result.push(6);
 
-  return answer;
+  return result;
+}
+
+// Solution 3
+function getRank(matchingNumberCount) {
+  let rank;
+
+  switch (matchingNumberCount) {
+    case 6:
+      rank = 1;
+      break;
+    case 5:
+      rank = 2;
+      break;
+    case 4:
+      rank = 3;
+      break;
+    case 3:
+      rank = 4;
+      break;
+    case 2:
+      rank = 5;
+      break;
+    default:
+      rank = 6;
+      break;
+  }
+
+  return rank;
+}
+
+function solution3(lottos, win_nums) {
+  const matchingNumberCount = lottos.filter((number) =>
+    win_nums.includes(number)
+  ).length;
+  const zeroCount = lottos.filter((number) => number === 0).length;
+  const result = [
+    getRank(matchingNumberCount + zeroCount),
+    getRank(matchingNumberCount),
+  ];
+
+  return result;
 }
