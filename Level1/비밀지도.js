aeb305447e510eb61f27d9627f940ef1905c6cf3
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-12-09 17:00:08 +0900

    Solve : 비밀지도

--- Level1/비밀지도.js
+++ Level1/비밀지도.js
@@ -1,21 +1,20 @@
-function solution(n, arr1, arr2) {
-  let finalMap = [];
-  const map1 = [];
-  const map2 = [];
+// Solution 1
+function solution1(n, arr1, arr2) {
+  const combinedMap = [];
+  const map1 = arr1.map((row) => row.toString(2).padStart(n, '0').split(''));
+  const map2 = arr2.map((row) => row.toString(2).padStart(n, '0').split(''));
 
   for (let i = 0; i < n; i++) {
-    const bin1 = arr1[i].toString(2).padStart(n, '0').split('');
-    const bin2 = arr2[i].toString(2).padStart(n, '0').split('');
+    const temp = [];
 
-    map1.push(bin1);
-    map2.push(bin2);
+    for (let j = 0; j < n; j++) {
+      map1[i][j] === '1' || map2[i][j] === '1'
+        ? temp.push('#')
+        : temp.push(' ');
+    }
+
+    combinedMap.push(temp.join(''));
   }
 
-  finalMap = map1.map((item1, x) =>
-    item1
-      .map((item2, y) => (Number(item2) + Number(map2[x][y]) > 0 ? '#' : ' '))
-      .join('')
-  );
-
-  return finalMap;
+  return combinedMap;
 }
