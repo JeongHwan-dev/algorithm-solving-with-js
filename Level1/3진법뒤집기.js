b23c91b67f2320ca9d22231eb42b0b4199bad96c
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-12-11 22:44:52 +0900

    Solve : 3진법 뒤집기

--- Level1/3진법뒤집기.js
+++ Level1/3진법뒤집기.js
@@ -1,5 +1,4 @@
 // Solution 1
-// split() 메서드를 사용한 솔루션
 function solution1(n) {
   const convertedNum = parseInt(n.toString(3).split('').reverse().join(''), 3);
 
@@ -7,7 +6,6 @@ function solution1(n) {
 }
 
 // Solution 2
-// 전개 연산자를 사용한 솔루션
 function solution2(n) {
   const convertedNum = parseInt([...n.toString(3)].reverse().join(''), 3);
 
