ac1c9555f95aeec93131b18cb27bbff25af8cd6b
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-12-16 21:27:05 +0900

    Solve : 폰켓몬

--- Level1/폰켓몬.js
+++ Level1/폰켓몬.js
@@ -1,5 +1,4 @@
 // Solution 1
-// Map 객체를 이용한 솔루션
 function solution1(nums) {
   const map = new Map();
   const count = parseInt(nums.length / 2);
@@ -18,20 +17,11 @@ function solution1(nums) {
 }
 
 // Solution 2
-// Set 객체를 이용한 솔루션
 function solution2(nums) {
-  const set = new Set(nums);
-  const count = parseInt(nums.length / 2);
-  const maxCount = set.size > count ? count : set.size;
+  const pokemonSet = new Set(nums);
+  const halfCount = nums.length / 2;
+  const maxTypeCount =
+    pokemonSet.size > halfCount ? halfCount : pokemonSet.size;
 
-  return maxCount;
-}
-
-// Solution 3
-// Set 객체를 이용한 솔루션
-function solution3(nums) {
-  const set = new Set(nums);
-  const maxCount = nums.length / 2;
-
-  return set.size > maxCount ? maxCount : set.size;
+  return maxTypeCount;
 }
