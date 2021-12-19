2c21de136196b07de50ba061b1390300193ca19b
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-12-19 19:16:17 +0900

    Solve : 피보나치 수

--- Level2/피보나치수.js
+++ Level2/피보나치수.js
@@ -1,9 +1,12 @@
-function solution(n) {
+// Solution 1
+function solution1(n) {
   const nums = [0, 1];
 
   for (let i = 2; i <= n; i++) {
     nums.push((nums[i - 2] + nums[i - 1]) % 1234567);
   }
 
-  return nums[n];
+  const remainder = nums[n];
+
+  return remainder;
 }
