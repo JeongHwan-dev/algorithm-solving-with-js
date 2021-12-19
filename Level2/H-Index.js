9396ec9a77103f341da196742266acea076759f0
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-12-19 19:14:13 +0900

    Solve : H-Index

--- Level2/H-Index.js
+++ Level2/H-Index.js
@@ -1,23 +1,18 @@
-function solution(citations) {
-  // 초기 인덱스 0으로 설정
-  let idx = 0;
+// Solution 1
+function solution1(citations) {
+  const _citations = citations.slice();
+  const n = _citations.length;
+  let HIndex = 0;
 
-  // 논문 인용 배열을 내림차순으로 정렬
-  citations.sort((a, b) => b - a);
+  _citations.sort((a, b) => b - a);
 
-  // 논문 인용 배열 길이 만큼 반복
-  while (idx <= citations.length) {
-    // 논문 인용 횟수가 현재 인용 횟수보다 작거나 같다면
-    if (idx + 1 <= citations[idx]) {
-      // 인덱스를 1씩 증가
-      idx++;
-    }
-    // 만약 현재 인용 횟수가 논문 인용 횟수보다 크다면
-    else {
-      // 반복문 종료
+  while (HIndex <= n) {
+    if (HIndex + 1 <= _citations[HIndex]) {
+      HIndex++;
+    } else {
       break;
     }
   }
 
-  return idx;
+  return HIndex;
 }
