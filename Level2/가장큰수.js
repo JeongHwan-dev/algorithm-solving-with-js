4d4448a7515e20b561b4abe76a239d442a256c23
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-12-19 19:01:09 +0900

    Solve : 가장 큰 수

--- Level2/가장큰수.js
+++ Level2/가장큰수.js
@@ -1,12 +1,18 @@
-function solution(numbers) {
-  let answer = '';
+// Solution 1
+function solution1(numbers) {
+  const _numbers = numbers.slice();
+  const maxNumber = _numbers
+    .map((number) => String(number))
+    .sort((a, b) => b + a - (a + b))
+    .join('');
 
-  answer = numbers
-    .map((n) => String(n)) // map() 메서드를 이용해 배열의 모든 수들을 문자열로 변경
-    .sort((a, b) => b + a - (a + b)) // 문자열 형태로 합쳐진 상태를 기준으로 내림차순으로 정렬
-    .join(''); // join() 메서드를 통해 배열의 숫자를 모두 합치기
+  return maxNumber[0] === '0' ? '0' : maxNumber;
+}
 
-  // 결과값의 맨 앞자리가 0이라면 0으로 리턴
-  // 그렇지 않다면 기존 결과값 리턴
-  return answer[0] === '0' ? '0' : answer;
+// Solution 2
+function solution2(numbers) {
+  const _numbers = numbers.slice();
+  const maxNumber = _numbers.sort((a, b) => `${b}${a}` - `${a}${b}`).join('');
+
+  return maxNumber[0] === '0' ? '0' : maxNumber;
 }
