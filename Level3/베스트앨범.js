c6eca8778e7c5ad195d61766375c9c065d02b5b4
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-12-25 22:56:19 +0900

    Solve : 베스트앨범

--- Level3/베스트앨범.js
+++ Level3/베스트앨범.js
@@ -1,4 +1,4 @@
-function solution(genres, plays) {
+function solution1(genres, plays) {
   const album = [];
   const map = new Map();
   const info = genres.map((value, index) => [value, plays[index], index]);
@@ -44,4 +44,28 @@ function solution(genres, plays) {
     });
 
   return album;
+}
+
+function solution2(genres, plays) {
+  const genreMap = new Map();
+
+  genres
+    .map((genre, index) => [genre, plays[index]])
+    .forEach(([genre, play], index) => {
+      const data = genreMap.get(genre) || { total: 0, songs: [] };
+
+      genreMap.set(genre, {
+        total: data.total + play,
+        songs: [...data.songs, { play, index }]
+          .sort((a, b) => b.play - a.play)
+          .slice(0, 2),
+      });
+    });
+
+  const order = [...genreMap.entries()]
+    .sort((a, b) => b[1].total - a[1].total)
+    .flatMap((item) => item[1].songs)
+    .map((song) => song.index);
+
+  return order;
 }
