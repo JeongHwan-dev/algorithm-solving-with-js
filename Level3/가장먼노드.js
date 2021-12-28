1a375374b2df50868ac0284a0dce69f3c39da568
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-12-28 23:28:33 +0900

    Solve : 가장 먼 노드

--- Level3/가장먼노드.js
+++ Level3/가장먼노드.js
@@ -33,3 +33,98 @@ function solution1(n, edge) {
 
   return maxLevelCount;
 }
+
+// Solution 2
+function solution2(n, edge) {
+  const graph = Array.from(Array(n + 1), () => []);
+
+  for (const [src, dest] of edge) {
+    graph[src].push(dest);
+    graph[dest].push(src);
+  }
+
+  const distance = Array(n + 1).fill(0);
+
+  distance[1] = 1;
+
+  const queue = [1];
+
+  while (queue.length > 0) {
+    const src = queue.shift();
+
+    for (const dest of graph[src]) {
+      if (distance[dest] === 0) {
+        queue.push(dest);
+        distance[dest] = distance[src] + 1;
+      }
+    }
+  }
+
+  const maxDistance = Math.max(...distance);
+  const maxDistanceCount = distance.filter(
+    (value) => value === maxDistance
+  ).length;
+
+  return maxDistanceCount;
+}
+
+// Solution 3
+class Queue {
+  constructor() {
+    this.queue = [];
+    this.front = 0;
+    this.rear = 0;
+  }
+
+  enqueue(value) {
+    this.queue[this.rear++] = value;
+  }
+
+  dequeue() {
+    const value = this.queue[this.front];
+
+    delete this.queue[this.front];
+    this.front += 1;
+
+    return value;
+  }
+
+  isEmpty() {
+    return this.front === this.rear;
+  }
+}
+
+function solution3(n, edge) {
+  const graph = Array.from(Array(n + 1), () => []);
+
+  for (const [src, dest] of edge) {
+    graph[src].push(dest);
+    graph[dest].push(src);
+  }
+
+  const distance = Array(n + 1).fill(0);
+
+  distance[1] = 1;
+
+  const queue = new Queue();
+
+  queue.enqueue(1);
+
+  while (!queue.isEmpty()) {
+    const src = queue.dequeue();
+
+    for (const dest of graph[src]) {
+      if (distance[dest] === 0) {
+        queue.enqueue(dest);
+        distance[dest] = distance[src] + 1;
+      }
+    }
+  }
+
+  const maxDistance = Math.max(...distance);
+  const maxDistanceCount = distance.filter(
+    (value) => value === maxDistance
+  ).length;
+
+  return maxDistanceCount;
+}
