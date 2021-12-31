d877bae636c7ad79876f5450dc9b2d19f6945112
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-12-31 23:51:46 +0900

    Solve : 보석 쇼핑

--- Level3/보석쇼핑.js
+++ Level3/보석쇼핑.js
@@ -1,4 +1,5 @@
-function solution(gems) {
+// Solution 1
+function solution1(gems) {
   const kind = new Set(gems).size;
   const gemMap = new Map();
   const sections = [];
@@ -20,4 +21,37 @@ function solution(gems) {
   );
 
   return sections[0];
+}
+
+// Solution 2
+function solution2(gems) {
+  let shortestSection = [0, gems.length];
+  let start = 0;
+  let end = 0;
+
+  const gemKinds = new Set(gems).size;
+  const collect = new Map();
+
+  collect.set(gems[start], 1);
+
+  while (start < gems.length && end < gems.length) {
+    if (collect.size === gemKinds) {
+      if (end - start < shortestSection[1] - shortestSection[0]) {
+        shortestSection = [start + 1, end + 1];
+      }
+
+      collect.set(gems[start], collect.get(gems[start]) - 1);
+
+      if (collect.get(gems[start]) === 0) {
+        collect.delete(gems[start]);
+      }
+
+      start += 1;
+    } else {
+      end += 1;
+      collect.set(gems[end], 1 + (collect.get(gems[end]) || 0));
+    }
+  }
+
+  return shortestSection;
 }
