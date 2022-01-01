1800b75131ba7fa6dda39a138101a1488efae4b7
JeongHwan-dev <jeonghwan.dev@gmail.com>
2022-01-01 23:01:05 +0900

    Solve : Breaking the Records

---
+++ Easy/BreakingTheRecords.js
@@ -0,0 +1,17 @@
+function breakingRecords(scores) {
+  const breakingRecordsCount = [0, 0];
+  let heighestScore = scores[0];
+  let lowestScore = scores[0];
+
+  for (let i = 1; i < scores.length; i++) {
+    if (scores[i] > heighestScore) {
+      breakingRecordsCount[0]++;
+      heighestScore = scores[i];
+    } else if (scores[i] < lowestScore) {
+      breakingRecordsCount[1]++;
+      lowestScore = scores[i];
+    }
+  }
+
+  return breakingRecordsCount;
+}
