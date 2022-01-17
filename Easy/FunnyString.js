9df7e5be563e0e301d42c65535580b9e5b2d7e66
JeongHwan-dev <jeonghwan.dev@gmail.com>
2022-01-17 18:33:28 +0900

    Solve : Funny String

---
+++ Easy/FunnyString.js
@@ -0,0 +1,22 @@
+function funnyString(s) {
+  const ascii = [];
+  const gap = [];
+
+  for (let i = 0; i < s.length; i++) {
+    ascii.push(s.charCodeAt(i));
+  }
+
+  for (let i = 0; i < ascii.length - 1; i++) {
+    gap.push(Math.abs(ascii[i] - ascii[i + 1]));
+  }
+
+  const reversedGap = gap.slice().reverse();
+
+  for (let i = 0; i < gap.length; i++) {
+    if (gap[i] !== reversedGap[i]) {
+      return 'NOT Funny';
+    }
+  }
+
+  return 'Funny';
+}
