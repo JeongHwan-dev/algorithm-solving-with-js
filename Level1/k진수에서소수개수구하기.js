0a205fd96ac8dae86dee0c496a3e1b49e0a2db39
JeongHwan-dev <jeonghwan.dev@gmail.com>
2022-01-30 13:03:09 +0900

    Solve : k진수에서 소수 개수 구하기

---
+++ Level1/k진수에서소수개수구하기.js
@@ -0,0 +1,23 @@
+function isPrimeNumber(number) {
+  if (number <= 1) {
+    return false;
+  }
+
+  for (let i = 2; i <= parseInt(Math.sqrt(number)); i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function solution(n, k) {
+  const convertedNum = n.toString(k);
+  const primeNumberCount = convertedNum
+    .split('0')
+    .filter((num) => num !== '')
+    .filter((num) => isPrimeNumber(parseInt(num))).length;
+
+  return primeNumberCount;
+}
