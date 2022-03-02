2296f7ca4983d887ddd0a9be7afc1e0593438efa
JeongHwan-dev <jeonghwan.dev@gmail.com>
2022-03-02 18:12:40 +0900

    Solve : 수들의 합 2

---
+++ Silver/BOJ2003.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const n = Number(input[0].split(' ')[0]);
+const m = Number(input[0].split(' ')[1]);
+const nums = input[1].split(' ').map((num) => Number(num));
+
+// Solution 1
+console.log(solution(n, m, nums));
+
+function solution(n, m, nums) {
+  let numberOfCases = 0;
+
+  for (let i = 0; i < n; i++) {
+    let index = i;
+    let sum = nums[i];
+
+    while (sum < m && index < n - 1) {
+      index += 1;
+      sum += nums[index];
+    }
+
+    if (sum === m) {
+      numberOfCases += 1;
+    }
+  }
+
+  return numberOfCases;
+}
