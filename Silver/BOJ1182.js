0dc6f211b3e7f604a25d722610282e5e66a6f3cf
JeongHwan-dev <jeonghwan.dev@gmail.com>
2022-03-02 15:48:37 +0900

    Solve : 부분수열의 합

---
+++ Silver/BOJ1182.js
@@ -0,0 +1,53 @@
+// Solution 1
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const [n, s] = input[0].split(' ').map((item) => Number(item));
+const arr = input[1].split(' ').map((item) => Number(item));
+
+// Solution 1
+console.log(solution1(n, s, arr));
+
+function solution1(n, s, arr) {
+  let numberOfCases = 0;
+
+  function dfs(l, sum) {
+    if (l === n) {
+      return;
+    }
+
+    sum += arr[l];
+
+    if (sum === s) {
+      numberOfCases += 1;
+    }
+
+    dfs(l + 1, sum);
+    dfs(l + 1, sum - arr[l]);
+  }
+
+  dfs(0, 0);
+
+  return numberOfCases;
+}
+
+// Solution 2
+console.log(solution2(n, s, arr));
+
+function solution2(n, s, arr) {
+  let numberOfCases = 0;
+
+  function dfs(l, sum, str) {
+    if (l === n) {
+      if (sum === s) {
+        numberOfCases += 1;
+      }
+    } else {
+      dfs(l + 1, sum + arr[l], str + `+${arr[l]}`);
+      dfs(l + 1, sum, str);
+    }
+  }
+
+  dfs(0, 0, '');
+
+  return s === 0 ? numberOfCases - 1 : numberOfCases;
+}
