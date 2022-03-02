e1844f1d69958d61bb946aa5f53fca8d46e5ca2d
JeongHwan-dev <jeonghwan.dev@gmail.com>
2022-03-02 13:26:00 +0900

    Solve : 1, 2, 3 더하기

---
+++ Silver/BOJ9095.js
@@ -0,0 +1,25 @@
+// Solution 1
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+const T = input[0];
+const data = [];
+
+for (let i = 1; i < input.length; i++) {
+  data.push(input[i]);
+}
+
+for (let i = 0; i < T; i++) {
+  const result = solution(data[i]);
+
+  console.log(result);
+}
+
+function solution(n) {
+  const dp = [0, 1, 2, 4];
+
+  for (let i = 4; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+  }
+
+  return dp[n];
+}
