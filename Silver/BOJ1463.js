f75dad32468da5c61770486c3c373d8679db9717
JeongHwan-dev <jeonghwan.dev@gmail.com>
2022-03-02 17:33:23 +0900

    Solve : 1로 만들기

---
+++ Silver/BOJ1463.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString();
+const n = Number(input);
+
+// Solution 1
+console.log(solution(n));
+
+function solution(n) {
+  let minOperationCount = Number.MAX_SAFE_INTEGER;
+
+  function dfs(l, result) {
+    if (l >= minOperationCount) {
+      return;
+    }
+
+    if (result === 1) {
+      minOperationCount = Math.min(minOperationCount, l);
+    } else {
+      if (result % 3 === 0) {
+        dfs(l + 1, result / 3);
+      }
+
+      if (result % 2 === 0) {
+        dfs(l + 1, result / 2);
+      }
+
+      dfs(l + 1, result - 1);
+    }
+  }
+
+  dfs(0, n);
+
+  return minOperationCount;
+}
