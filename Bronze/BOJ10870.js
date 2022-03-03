25e81f326c278f7150ab2bf90c647232dfba90cf
JeongHwan-dev <jeonghwan.dev@gmail.com>
2022-03-03 21:06:20 +0900

    Solve : 피보나치 수 5

---
+++ Bronze/BOJ10870.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim();
+const n = parseInt(input);
+
+console.log(solution(n));
+
+function getFibonacci(n) {
+  const fibonacci = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    fibonacci.push(fibonacci[i - 2] + fibonacci[i - 1]);
+  }
+
+  return fibonacci;
+}
+
+function solution(n) {
+  const fibonacci = getFibonacci(n);
+
+  return fibonacci[n];
+}
