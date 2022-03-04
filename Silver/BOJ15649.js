b42a5ad0cd39d9619f425a31c2228c8d7d2a94e5
JeongHwan-dev <jeonghwan.dev@gmail.com>
2022-03-04 17:41:50 +0900

    Solve : N과 M (1)

---
+++ Silver/BOJ15649.js
@@ -0,0 +1,93 @@
+// Solution 1
+function getPermutations(array, selectNumber) {
+  const results = [];
+
+  if (selectNumber === 1) {
+    return array.map((item) => [item]);
+  }
+
+  array.forEach((fixed, index, origin) => {
+    const rest = [...origin.slice(0, index), ...origin.slice(index + 1)];
+    const permutations = getPermutations(rest, selectNumber - 1);
+    const attached = permutations.map((permutation) => [fixed, ...permutation]);
+
+    results.push(...attached);
+  });
+
+  return results;
+}
+
+function solution1(n, m) {
+  const array = Array.from({ length: n }, (_, index) => index + 1);
+  const permutations = getPermutations(array, m);
+  let result = '';
+
+  for (const permutation of permutations) {
+    result += permutation.join(' ') + '\n';
+  }
+
+  return result;
+}
+
+const fs = require('fs');
+const input = fs
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split(' ')
+  .map((value) => parseInt(value));
+const n = input[0];
+const m = input[1];
+
+console.log(solution1(n, m));
+
+// Solution 2
+function getPermutations(array, selectNumber) {
+  const len = array.length;
+  const permutations = [];
+  const permutation = Array.from({ length: selectNumber }, () => 0);
+  const visited = Array.from({ length: len }, () => false);
+
+  function dfs(l) {
+    if (l === selectNumber) {
+      permutations.push(permutation.slice());
+    } else {
+      for (let i = 0; i < len; i++) {
+        if (!visited[i]) {
+          visited[i] = true;
+          permutation[l] = array[i];
+          dfs(l + 1);
+          visited[i] = false;
+        }
+      }
+    }
+  }
+
+  dfs(0);
+
+  return permutations;
+}
+
+function solution2(n, m) {
+  const array = Array.from({ length: n }, (_, index) => index + 1);
+  const permutations = getPermutations(array, m);
+  let result = '';
+
+  for (const permutation of permutations) {
+    result += permutation.join(' ') + '\n';
+  }
+
+  return result;
+}
+
+const fs = require('fs');
+const input = fs
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split(' ')
+  .map((value) => parseInt(value));
+const n = input[0];
+const m = input[1];
+
+console.log(solution2(n, m));
