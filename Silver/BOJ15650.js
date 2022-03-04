6604fd142f9b3498973cbf3af6f7d64c107db74b
JeongHwan-dev <jeonghwan.dev@gmail.com>
2022-03-04 18:01:41 +0900

    Solve : N과 M (2)

---
+++ Silver/BOJ15650.js
@@ -0,0 +1,87 @@
+// Solution 1
+function getCombinations(array, selectNumber) {
+  const results = [];
+
+  if (selectNumber === 1) {
+    return array.map((item) => [item]);
+  }
+
+  array.forEach((fixed, index, origin) => {
+    const rest = origin.slice(index + 1);
+    const combinations = getCombinations(rest, selectNumber - 1);
+    const attached = combinations.map((combination) => [fixed, ...combination]);
+
+    results.push(...attached);
+  });
+
+  return results;
+}
+
+function solution1(n, m) {
+  const array = Array.from({ length: n }, (_, index) => index + 1);
+  const combinations = getCombinations(array, m);
+  let result = '';
+
+  for (const combination of combinations) {
+    result += combination.join(' ') + '\n';
+  }
+
+  return result;
+}
+
+const fs = require('fs');
+const input = fs
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split(' ')
+  .map((value) => parseInt(value));
+const n = input[0];
+const m = input[1];
+
+console.log(solution1(n, m));
+
+// Solution 2
+function getCombinations(array, selectNumber) {
+  const combinations = [];
+  const combination = Array.from({ length: selectNumber }, () => 0);
+
+  function dfs(l, start) {
+    if (l === selectNumber) {
+      combinations.push(combination.slice());
+    } else {
+      for (let i = start; i < array.length; i++) {
+        combination[l] = array[i];
+        dfs(l + 1, i + 1);
+      }
+    }
+  }
+
+  dfs(0, 0);
+
+  return combinations;
+}
+
+function solution2(n, m) {
+  const array = Array.from({ length: n }, (_, index) => index + 1);
+  const combinations = getCombinations(array, m);
+  let result = '';
+
+  for (const combination of combinations) {
+    result += combination.join(' ') + '\n';
+  }
+
+  return result;
+}
+
+const fs = require('fs');
+const input = fs
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split(' ')
+  .map((value) => parseInt(value));
+const n = input[0];
+const m = input[1];
+
+console.log(solution2(n, m));
