41af66d9e680f73c6f7a8d126993503029218b64
JeongHwan-dev <jeonghwan.dev@gmail.com>
2022-03-09 00:47:05 +0900

    Solve : 수 찾기

---
+++ Silver/BOJ1920.js
@@ -0,0 +1,25 @@
+function solution(nArray, mArray) {
+  const nMap = new Map();
+  const results = [];
+
+  for (const num of nArray) {
+    if (!nMap.has(num)) {
+      nMap.set(num, 1);
+    }
+  }
+
+  for (const num of mArray) {
+    nMap.has(num) ? results.push(1) : results.push(0);
+  }
+
+  return results.join('\n');
+}
+
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const n = parseInt(input[0]);
+const nArray = input[1].split(' ').map((value) => parseInt(value));
+const m = parseInt(input[2]);
+const mArray = input[3].split(' ').map((value) => parseInt(value));
+
+console.log(solution(nArray, mArray));
