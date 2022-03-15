168579e699bc73e452c6e652634235fe2c498473
JeongHwan-dev <jeonghwan.dev@gmail.com>
2022-03-15 20:50:23 +0900

    Solve : 1, 2, 3 더하기

--- Silver/BOJ9095.js
+++ Silver/BOJ9095.js
@@ -1,5 +1,5 @@
 // Solution 1
-function solution(n) {
+function solution1(n) {
   const dp = [0, 1, 2, 4];
 
   for (let i = 4; i <= n; i++) {
@@ -7,6 +7,33 @@ function solution(n) {
   }
 
   return dp[n];
+}
+
+// Solution 2
+function getCombinationCount(num) {
+  const dp = Array.from({ length: num + 1 }, () => 0);
+
+  dp[1] = 1;
+  dp[2] = 2;
+  dp[3] = 4;
+
+  for (let i = 4; i <= num; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+  }
+
+  return dp[num];
+}
+
+function solution2(nums) {
+  const results = [];
+
+  for (const num of nums) {
+    const combinationCount = getCombinationCount(num);
+
+    results.push(combinationCount);
+  }
+
+  return results.join('\n');
 }
 
 const fs = require('fs');
@@ -19,7 +46,9 @@ for (let i = 1; i < input.length; i++) {
 }
 
 for (let i = 0; i < T; i++) {
-  const result = solution(data[i]);
+  const result = solution1(data[i]);
 
   console.log(result);
 }
+
+console.log(solution2(data));
