fe5facb7ff9e0b46e05a9c3ec01b9e42a46f0261
박정환/서비스프로토타입개발팀 <jeonghwan.dev@gmail.com>
2023-11-25 23:37:58 +0900

    solve: 특별한 이차원 배열 2

---
+++ Level0/PRO181831.js
@@ -0,0 +1,12 @@
+// Solution 1
+function solution(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if (arr[i][j] !== arr[j][i]) {
+        return 0;
+      }
+    }
+  }
+
+  return 1;
+}
